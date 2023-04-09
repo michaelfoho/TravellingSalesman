849cb2bed98b6f8711dd99f97f9485e036c978d3
Hossein <michaelfaho@gmail.com>
2023-04-09 21:25:08 +0330

    start

--- src/TreasureLoot.java
+++ src/TreasureLoot.java
@@ -1,14 +1,13 @@
 public class TreasureLoot {
-
     int x;
     int y;
     int price;
 
     TreasureLoot(int x, int y, int price) {
+
         this.x = x;
         this.y = y;
         this.price = price;
+
     }
-
-
 }
