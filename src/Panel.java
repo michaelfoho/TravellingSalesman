538c49dc1cdff9d8ba4d137a84422dd69fa0ef70
Hossein <michaelfaho@gmail.com>
2023-04-09 21:10:53 +0330

    start

--- src/Panel.java
+++ src/Panel.java
@@ -275,7 +275,7 @@ public class Panel extends JPanel implements ActionListener {
 
                 g2D.setPaint(Color.BLUE);
                 g2D.drawString(String.valueOf(playerInfo.power), 1060, 200);
-                g2D.setPaint(new Color(190, 150, 0));
+                g2D.setPaint(new Color(20, 130, 0));
                 g2D.drawString(String.valueOf(playerInfo.money), 1060, 245);
 
 
